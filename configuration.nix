2208c7649169e19a9ed902d1188caa52954c3d5c
hitalin <hitalin@proton.me>
2024-11-06 17:42:55 +0900

    fix: Failed to start Network Manager Wait Online., use wpa_supplicant instead to avoid error

--- configuration.nix
+++ configuration.nix
@@ -17,8 +17,8 @@
 
   networking.hostName = "melchior"; # Define your hostname.
   # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Asia/Tokyo";
