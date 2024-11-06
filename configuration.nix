3a147b75e03803f9154d46e1d77888ff0abdbbd4
hitalin <hitalin@proton.me>
2024-11-06 17:39:50 +0900

    fix: Failed to start Network Manager Wait Online.

--- configuration.nix
+++ configuration.nix
@@ -19,7 +19,6 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-  systemd.services.NetworkManager-wait-online.serviceConfig.TimeoutStartSec = "2min";
 
   # Set your time zone.
   time.timeZone = "Asia/Tokyo";
